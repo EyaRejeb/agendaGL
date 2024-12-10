3da4a981b3c28fac20adae0ad61f0aacbaf54d4d
EyaRejeb <eyarejeb02@gmail.com>
2024-12-10 16:41:30 +0100

    ajout du code

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -7,14 +7,14 @@ import java.util.*;
  * Description : An agenda that stores events
  */
 public class Agenda {
+    ArrayList<Event> events = new ArrayList<>();
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
     public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        events.add(e);
     }
 
     /**
@@ -24,7 +24,41 @@ public class Agenda {
      * @return a list of events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        ArrayList<Event> todayEvents = new ArrayList<Event>();
+        for (Event e : events) {
+            if (e.isInDay(day)){
+                todayEvents.add(e);
+            }
+        }
+        return todayEvents;
+    }
+
+    /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) {
+        ArrayList<Event> eventsDay = new ArrayList<>();
+        for (Event e : events) {
+            if (e.getTitle().equals(title)) {
+                eventsDay.add(e);
+            }
+        }
+        return eventsDay;
+    }
+    
+    /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+        for (Event event : events) {
+            if (e.getStart().plus(e.getDuration()).isAfter(event.getStart()) && e.getStart().isBefore(event.getStart().plus(event.getDuration()))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/agenda/Repetition.java
+++ src/main/java/agenda/Repetition.java
@@ -1,10 +1,8 @@
 package agenda;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Repetition {
     public ChronoUnit getFrequency() {
@@ -20,6 +18,8 @@ public class Repetition {
      * </UL>
      */
     private final ChronoUnit myFrequency;
+    private final ArrayList<LocalDate> exceptions = new ArrayList<>();
+    private Termination termination;
 
     public Repetition(ChronoUnit myFrequency) {
         this.myFrequency = myFrequency;
@@ -30,8 +30,7 @@ public class Repetition {
      * @param date un date à laquelle l'événement ne doit pas se répéter
      */
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        exceptions.add(date);
     }
 
     /**
@@ -39,8 +38,25 @@ public class Repetition {
      * @param termination la terminaison de la répétition
      */
     public void setTermination(Termination termination) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
-
+        this.termination = termination;
     }
+
+    /**
+     * Vérifie si une date est marquée comme exception
+     * @param date une date à vérifier
+     * @return true si la date est une exception, false sinon
+     */
+    public boolean isException(LocalDate date) {
+        return exceptions.contains(date);
+    }
+
+    /**
+     * Retourne la terminaison associée à la répétition
+     * @return la terminaison de la répétition (ou null si aucune n'est définie)
+     */
+    public Termination getTermination() {
+        return termination;
+    }
+
+    
 }
